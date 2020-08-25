6c5cb8ec0d1c2652790a75bdb4412cb3448051c9
JuZihao <57383789+JuZihao@users.noreply.github.com>
2020-08-25 16:41:06 +0800

    Added greet function,echo function and bye function

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,8 +8,34 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        greet();
+        echo();
+        bye();
+    }
+
+    public static void greet() {
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?");
+    }
+
+    public static void echo(){
+        boolean checkForBye = false;
+        String input;
+        Scanner in = new Scanner(System.in);
+
+        while (!checkForBye) {
+            input = in.nextLine();
+            checkForBye = input.equals("bye");
+            if(checkForBye){
+                break;
+            }
+            else {
+                System.out.println(input);
+            }
+        }
+    }
+
+    public static void bye(){
         System.out.println("Bye! Hope to see you soon!");
     }
 }
