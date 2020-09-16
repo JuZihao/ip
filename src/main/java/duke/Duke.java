5119013f06d21a98e1a728945cb2985358b04e05
JuZihao <57383789+JuZihao@users.noreply.github.com>
2020-09-16 19:00:18 +0800

    Supports save file. Saves all the changes that happen to task list.

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -13,6 +13,8 @@ public class Duke {
         TaskList currentTaskList = new TaskList();
 
         greet();
+        currentTaskList.loadSavedTasks();
+
         while (true) {
             input = in.nextLine();
             AnalyseCommand userCommands = new AnalyseCommand(input);
@@ -26,6 +28,7 @@ public class Duke {
                 currentTaskList.addTask(userCommands);
             }
         }
+        currentTaskList.saveTasksAsText();
         bye();
     }
 }
